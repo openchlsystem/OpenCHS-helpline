a55374c2ac21db5be11df1be9104aedf685b894b
mimikimani <josephkimanibitz@gmail.com>
2024-01-18 14:41:58 +0300

    ati.js,activity.js similar to tower branch

---
+++ src/hh18jan24/app/disposition.js
@@ -0,0 +1,25 @@
+
+
+te["disposition_k"] = { c:
+[
+	{ arg:["","src_address",":k:dispositions_k:src_address:2"] },
+
+	{ arg:["","reporter_contact_id",":k:dispositions_k:reporter_contact_id:2"] },
+	{ arg:["","reporter_fullname",":k:dispositions_k:reporter_fullname:2"] },
+	{ arg:["","reporter_phone",":k:dispositions_k:reporter_phone:2"] },
+	{ arg:["","reporter_age_group_id",":k:dispositions_k:reporter_age_group_id:2"] },
+	{ arg:["","reporter_sex_id",":k:dispositions_k:reporter_sex_id:2"] },
+	{ arg:["","reporter_location_id",":k:dispositions_k:reporter_location_id:2"] },
+	
+	{ arg:["","cases^id",":k:dispositions_k:cases^id:2"] },
+	{ arg:["","cases^created_on",":k:dispositions_k:cases^created_on:2"] },
+	{ arg:["","cases^created_by_id",":k:dispositions_k:cases^created_by_id:2"] },
+	{ arg:["","cases^src",":k:dispositions_k:cases^src:2"] },
+	{ arg:["","cases^case_category_id",":k:dispositions_k:cases^case_category_id:2"] },
+        { arg:["","cases^gbv_related",":k:dispositions_k:cases^gbv_related:2"] },
+	{ arg:["","cases^priority",":k:dispositions_k:cases^priority:2"] },
+	{ arg:["","cases^status",":k:dispositions_k:cases^status:2"] },
+	{ arg:["","cases^escalated_to_id",":k:dispositions_k:cases^escalated_to_id:2"] },
+	{ arg:["","cases^assessment_id",":k:dispositions_k:cases^assessment_id:2"] },
+	{ arg:["","cases^justice_id",":k:dispositions_k:cases^justice_id:2"] },
+]};
